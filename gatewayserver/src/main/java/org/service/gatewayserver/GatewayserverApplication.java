7d9241a04ae37fa1b9ce02c67897d897e24f460a
prakashponali <p.ponali@gamil.com>
2024-01-11 11:40:13 +0530

    section 10 initial changes

--- gatewayserver/src/main/java/org/service/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/org/service/gatewayserver/GatewayserverApplication.java
@@ -2,7 +2,11 @@ package org.service.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
